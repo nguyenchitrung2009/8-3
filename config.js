c24dcd66e5bfbc1edd2bb0c473324c7257ffa3ca
BaoBao79 <101114804+BaoBao79@users.noreply.github.com>
2022-03-07 23:07:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Hi Quỳnh Anh😘",
-    introTitle: 'Bảo Bảo <333',
+    titleWeb: "Bảo Bảo😘",
+    introTitle: 'HI Quỳnh Anh 😘',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên🥰Ngồi chờ duyên phận ý trời',
+    desc: 'Phải chăng Chi bằng chủ động ngỏ lời thương nhau! ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Học sâu, hiểu rộng, biết nhiều. Chi bằng gặp mặt đánh liều yêu cậu! <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'chúc cậu mùng 8/3 vui vẻ ngày càng xinh đẹp trong mắt tớ nhaaaaa, iu cậu nhìu ạ <3.',
+    messDesc: 'chúc cậu mùng 8/3 vui vẻ ngày càng xinh đẹp trong mắt tớ nhaaaaa, iu cậu nhìu ạ <33333.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/profile.php?id=100009002614810' 
+    messLink: https://www.facebook.com/dii.hanthien.90857' 
 }
